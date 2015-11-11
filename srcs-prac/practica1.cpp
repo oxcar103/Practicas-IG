9bb84e7010519ab5ee0ca11bbe80f854b3cb70c3
Óscar Bermúdez Garrido <oxcar103@gmail.com>
2015-11-11 19:34:37 +0100

    Contenidos proporcionados por el profesor

---
+++ srcs-prac/practica1.cpp
@@ -0,0 +1,55 @@
+// *********************************************************************
+// **
+// ** Informática Gráfica, curso 2015-16
+// ** Práctica 1  (implementación)
+// **
+// *********************************************************************
+
+#include "aux.hpp"
+#include "tuplasg.hpp"   // Tupla3f 
+#include "practica1.hpp"
+
+unsigned objeto_activo = 0 ; // objeto activo: cubo (0), tetraedro (1), otros....
+
+// ---------------------------------------------------------------------
+// declaraciones de estructuras de datos....
+
+
+
+// ---------------------------------------------------------------------
+// Función para implementar en la práctica 1 para inicialización.
+// Se llama una vez al inicio, cuando ya se ha creado la ventana e 
+// incializado OpenGL. 
+
+void P1_Inicializar( int argc, char *argv[] )
+{
+   
+   
+}
+
+// ---------------------------------------------------------------------
+// Función invocada al pulsar una tecla con la práctica 1 activa:
+// (si la tecla no se procesa en el 'main').
+//
+//  - devuelve 'true' si la tecla se usa en esta práctica para cambiar 
+//    entre el cubo, el tetraedro u otros objetos (cambia el valor de
+//    'objeto_activo').
+//  - devuelve 'false' si la tecla no se usa en esta práctica (no ha
+//    cambiado nada)
+
+bool P1_FGE_PulsarTeclaNormal( unsigned char tecla ) 
+{
+   // ........
+   return false ;
+}
+
+
+// ---------------------------------------------------------------------
+// Función a implementar en la práctica 1  para dibujar los objetos
+// modo: 0 - puntos, 1 - alambre, 2 - sólido, 3 - sólido ajedrez , >=4 otros....
+
+void P1_DibujarObjetos( unsigned modo ) 
+{
+  
+
+}
---
+++ srcs-prac/practica1.hpp
@@ -0,0 +1,15 @@
+// *********************************************************************
+// **
+// ** Informática Gráfica, curso 2015-16
+// ** Práctica 1  (declaraciones públicas)
+// **
+// *********************************************************************
+
+#ifndef IG_PRACTICA1_HPP
+#define IG_PRACTICA1_HPP
+
+void P1_Inicializar( int argc, char *argv[] ) ;
+bool P1_FGE_PulsarTeclaNormal(  unsigned char tecla ) ;
+void P1_DibujarObjetos( unsigned modo ) ; 
+
+#endif
