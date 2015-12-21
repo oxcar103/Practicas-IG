058f8214aa18baae92794a152e85f8c87296e6cb
Óscar Bermúdez Garrido <oxcar103@gmail.com>
2015-12-21 10:40:34 +0100

    Implementada practica3.cpp

--- srcs-prac/practica3.cpp
+++ srcs-prac/practica3.cpp
@@ -10,13 +10,64 @@
 #include "practica3.hpp"
 #include "MallasInd.hpp" 
 #include <vector>
+#include "Escena.hpp"
+#include "FreedomDegree.hpp"
 
 static unsigned objeto_activo;
+static unsigned grado_activo;
 
 // ---------------------------------------------------------------------
 // declaraciones de estructuras de datos....
 
-static MallaInd * * figuras;
+static NodoGrafoEscena * * figuras;
+Helicopter * chopper;
+FreedomDegree<int> *subir, *avanzar;
+FreedomDegree<double> *helice, *cola;
+
+// ---------------------------------------------------------------------
+// Funciones auxiliares
+
+void aumentar(){
+    switch(grado_activo){
+        case 0:
+            subir->increase();
+            chopper->setY(subir->get_value());
+            break;
+        case 1:
+            avanzar->increase();
+            chopper->setX(avanzar->get_value());
+            break;
+        case 2:
+            helice->increase();
+            chopper->setAngleHRotor(helice->get_value());
+            break;
+        case 3:
+            cola->increase();
+            chopper->setAngleTRotor(cola->get_value());
+            break;
+    }
+}
+
+void disminuir(){
+    switch(grado_activo){
+        case 0:
+            subir->decrease();
+            chopper->setY(subir->get_value());
+            break;
+        case 1:
+            avanzar->decrease();
+            chopper->setX(avanzar->get_value());
+            break;
+        case 2:
+            helice->decrease();
+            chopper->setAngleHRotor(helice->get_value());
+            break;
+        case 3:
+            cola->decrease();
+            chopper->setAngleTRotor(cola->get_value());
+            break;
+    }
+}
 
 // ---------------------------------------------------------------------
 // Función para implementar en la práctica 3 para inicialización.
@@ -24,6 +75,18 @@ static MallaInd * * figuras;
 // incializado OpenGL. 
 
 void P3_Inicializar(int argc, char *argv[]){
+    subir = new FreedomDegree<int> (0, 0, 50, 1, false);
+    avanzar = new FreedomDegree<int> (0, -30, 30);
+    helice = new FreedomDegree<double> (0, 0, 180, 5, true);
+    cola = new FreedomDegree<double> (0, 0, 180, 10);
+
+    grado_activo = 0;
+    
+    chopper = new Helicopter();
+    
+    figuras = new NodoGrafoEscena * [1] {chopper};
+
+    objeto_activo = 0;
 }
 
 // ---------------------------------------------------------------------
@@ -42,6 +105,18 @@ bool P3_FGE_PulsarTeclaNormal(unsigned char tecla){
             objeto_activo = (objeto_activo+1) % 1;
             break;
 
+        case 'G':
+            grado_activo = (grado_activo+1) % 4;
+            break;
+
+        case '<':
+            disminuir();
+            break;
+
+        case '>':
+            aumentar();
+            break;
+
         default:
             return false;
     }
@@ -54,6 +129,6 @@ bool P3_FGE_PulsarTeclaNormal(unsigned char tecla){
 // modo: 0 - puntos, 1 - alambre, 2 - sólido, 3 - sólido ajedrez , >=4 otros....
 
 void P3_DibujarObjetos(unsigned modo){
-//    figuras[objeto_activo]->visualizar(modo);
+    figuras[objeto_activo]->visualizar(modo);
 }
 
--- srcs-prac/practica3.hpp
+++ srcs-prac/practica3.hpp
@@ -8,6 +8,8 @@
 #ifndef IG_PRACTICA3_HPP
 #define IG_PRACTICA3_HPP
 
+#include <vector>
+
 void P3_Inicializar(int argc, char *argv[]);
 bool P3_FGE_PulsarTeclaNormal(unsigned char tecla);
 void P3_DibujarObjetos(unsigned modo); 
